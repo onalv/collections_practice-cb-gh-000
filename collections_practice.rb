77f03dbd6ee72908f13f34d58a7e94ff5c6debea
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-14 15:22:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,15 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    else
+      1
+    end
+  end
 end
 
 def sort_array_char_count(array)
